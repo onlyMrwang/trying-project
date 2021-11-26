7b9a4f7d6a56d579b2bbf64687cbfa20a00f7dd4
coop002 <wangjx@eastcom-sw.com>
2021-11-26 10:57:50 +0800

    arr手写

--- zhuawa-web面试题解析/index.js
+++ zhuawa-web面试题解析/index.js
@@ -63,18 +63,40 @@
 
 // 那Vue对于数组类型是怎么处理的？你能简单的那模拟一下对于数组方法的监听吗？要求最终输出如下方代码所示：
 
-const render = () => {
+const render = (action, ...args) => {
+  console.log(`Action = ${action},args=${args.join(",")} `)
+};
 
-}
+// 对于重写的数组方法，首先是要获取到它本来的方法，并保存一份
+const arrPrototype = Array.prototype;
+// 创建一个新的数组原型
+const newArrPrototype = Object.create(arrPrototype);
+
+// 遍历，修改新的数组原型数组上的每一个方法，就是赋值
+["push", "pop", "shift", "unshift", "sort", "splice", "reverse"].forEach((methodName) => {
+  // 先去执行数组原有的方法，不去更改this，哪个数组执行方法，就指向哪个
+  newArrPrototype[methodName] = function () {
+    console.log("------", this, ...arguments);//[1,2,3,4],指向的是操作的数组
+    // 就是一个赋值操作，将执行到的方法赋值给新定义的原型上
+    arrPrototype[methodName].call(this, ...arguments)
+    // 触发渲染
+    render(methodName, ...arguments)
+  }
+});
 
 const reactive = (obj) => {
+  // 判断是否是数组
+  if (Array.isArray(obj)) {
+    // 就把定义的新的原型指向obj.__proto__
+    obj.__proto__ = newArrPrototype
+  }
+};
 
-}
+const data = [1, 2, 3, 4];
+reactive(data);
 
-const data = [1, 2, 3, 4]
-
-reactive(data)
-
+data.push(5) // Action = push,args=5
+data.splice(0, 2) // Action = splice, args=0,2
 
 
 
